5ac8187ba9a46400d9468777de4d8e8ab91dc217
Rachit Sharma <s.rac98@gmail.com>
2021-09-22 10:01:22 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect,useRef} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const canvasRef = useRef(null);
+    const ctx = useRef(null);
+    const [isDrawing, setIsDrawing] = useState(false);
+
+    useEffect(()=>{
+        console.log(canvasRef.current)
+        canvasRef.current.width = window.innerWidth;
+        ctx.current = canvasRef.current.getContext('2d');
+        console.log(ctx.current)
+    },[])
+
+
+    function startDrawing(){
+        setIsDrawing(true)
+    }
+
+    function stopDrawing(){
+        setIsDrawing(false)
+        ctx.current.beginPath()
+    }
+
+    function draw(e){
+        const {clientX, clientY} = e;
+        if(!isDrawing) return;
+        ctx.current.strokeStyle = "black";
+        ctx.current.lineWidth = 5;
+        ctx.current.lineCap = "round";
+        ctx.current.lineTo(clientX,clientY)
+        ctx.current.stroke()
+        ctx.current.beginPath()
+        ctx.current.moveTo(clientX,clientY)
+    }
+
+    function clearDrawing(){
+        ctx.current.clearRect(0,0,ctx.current.canvas.width,ctx.current.canvas.height)
+    }
+
+    return (
+        <div className="app">
+            <canvas
+                ref={canvasRef} 
+                height={600}
+                onMouseDown={startDrawing}
+                onMouseUp={stopDrawing}
+                onMouseMove={draw}
+                style={{border: "2px solid black"}}
+            />
+            <button className="btn" onClick={clearDrawing}>Clear</button>
+        </div>
+    )
 }
 
 export default App;
